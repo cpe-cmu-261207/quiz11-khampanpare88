e5bac2c87d8f381e94874f9ae6fc6352271d95ce
khampanpare88 <khampanpare88@gmail.com>
2021-01-12 14:01:24 +0700

    1

--- app_template.js
+++ app_template.js
@@ -1,13 +1,4 @@
 //your code here!
 var num  = document.getElementById('input-number').innerHTML
-var mon = Number(620610777) - Number(num)
+var mon = Number(620610777) - num
 document.getElementById("num").innerHTML = String(mon)
-
-       // gpax = Number(gpax)
-       // var wx = document.getElementById("wx").innerHTML
-      //  wx = Number(wx)
-//gpax = Number(gpax)  * Number(wx)
-      //  wx = Number(wx) + Number(weight)
-      //  gpax = (Number(gpax) + Number(newList))/Number(wx)
-      //  document.getElementById("gpax").innerHTML = String(gpax)
-       // document.getElementById("wx").innerHTML = String(wx)
